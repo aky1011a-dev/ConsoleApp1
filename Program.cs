86d0e9bece564cad8fc201059d4442914bc41c76
Adem Akyol <Aky1011A@maidenerleghschools.co.uk>
2026-09-15 15:36:52 +0100

    bobs your uncle

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("bye world");
+//some test comments
+//hello
+
+
+
+
+
+//test again
